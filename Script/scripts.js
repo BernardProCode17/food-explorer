db20c0cac49c24f2e0d7e17291cf3862cc5add04
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2024-01-04 16:40:07 -0800

    restructured

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,10 +1,12 @@
 const menuToggle = document.getElementById('menu-toggle');
 const navigationMenu = document.getElementById('navigation-menu');
 menuToggle.addEventListener('click', function(){
+
+    navigationMenu.classList.toggle('show');
  
-    if(navigationMenu.style.display == 'none'){
-        navigationMenu.style.display = 'block';
-}else {
-    navigationMenu.style.display = 'none';
-}
-})
+    // if(navigationMenu.style.display == 'none'){
+    //     navigationMenu.style.display = 'block';
+    // }else {
+    //     navigationMenu.removeAttribute('style');
+    // }
+});
